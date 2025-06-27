91e8286a99c0f6315e2b08e9c9a0c2db2039e4e1
G Ben <gben@jinoMacBook-Air.local>
2025-06-28 05:51:45 +0900

    fix base path for coaching-web repository

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/coashing/",
+  base: "/coaching-web/",
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
   },
 });
